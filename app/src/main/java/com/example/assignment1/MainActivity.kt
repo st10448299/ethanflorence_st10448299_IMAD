3fe2ec2db160cf27ec2840d3d5e3ad2f0eb87678
FLO rider <st10448299@vcconnect.edu.za>
2024-04-05 08:59:31 +0200

    finished

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.assignment1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
